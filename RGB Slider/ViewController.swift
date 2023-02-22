52b812525f85b519d4526a8e24a254bcae35da4e
Viktor Teslenko <meex_v92@mail.ru>
2023-02-22 14:50:44 +0800

    Add names for color label

--- RGB Slider/ViewController.swift
+++ RGB Slider/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainView: UIView!
+
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
 
 
+    
+    
 }
 
